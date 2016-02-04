7aaae509774da612311ff86fbb0f059178b9a577
jmjuanes <josemijuanes@gmail.com>
2016-02-04 17:02:37 +0100

    Update gulpfile for add SASS compiler

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,28 @@ var fs = require('fs');
 var gulp = require('gulp');
 var ejs = require("gulp-ejs");
 var gulpFilter = require('gulp-filter');
-
-//Output dir
-var output = './';
+var sass = require('gulp-sass');
 
 //Include config
-var Config = require('./config.json');
+var Config = require('../siimple/package.json');
 
-//Build the website
-gulp.task('build', function(){
+//Output dir
+var output = '../siimple-website/';
+
+//Files list
+var paths = { 'src': ['src/*.ejs'], 'styles': ['scss/**/*'] };
+
+//Build the source website
+gulp.task('src', function(){
 
   //Set the filter
-  var filter = gulpFilter(['**/*', '!layouts/**/*', '!ref/**/*']);
+  //var filter = gulpFilter(['**/*', '!layouts/**/*', '!ref/**/*']);
 
   //Set the source folder
-  gulp.src('src/**/*.ejs')
+  gulp.src(paths.src)
 
   //Ignore layouts files
-  .pipe(filter)
+  //.pipe(filter)
 
   //Call the ejs builder
   .pipe(ejs(Config))
@@ -29,6 +33,23 @@ gulp.task('build', function(){
   .pipe(gulp.dest(output));
 
 });
+
+//Build css files
+gulp.task('styles', function(){
+
+	//Get all the scss files
+  gulp.src(paths.styles)
+
+	//Build the css files
+  .pipe(sass().on('error', sass.logError))
+
+	//Save to the output dir
+  .pipe(gulp.dest(output + 'css/'))
+
+});
+
+//Build task
+gulp.task('build', ['src', 'styles']);
 
 //Default
 gulp.task('default', ['build']);
